99341163e8be7ce12b3cbdd43d144c8112d30c65
REI BENESH <166454911+erbenesh@users.noreply.github.com>
2024-09-03 12:05:08 +0300

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+function onLinkClickAGame(event){
+    event.preventDefault();
+    document.querySelector('.about_game').scrollIntoView({ behavior: 'smooth' });
+}
+
+function onLinkClickReviews(event){
+    event.preventDefault();
+    document.querySelector('.reviews').scrollIntoView({ behavior: 'smooth' });
+}
+
+function onLinkClickContacts(event){
+    event.preventDefault();
+    document.querySelector('.order_form').scrollIntoView({ behavior: 'smooth' });
+}
+
+function onLinkClickButton(event){
+    event.preventDefault();
+    document.querySelector('.order_form').scrollIntoView({ behavior: 'smooth' });
+}
